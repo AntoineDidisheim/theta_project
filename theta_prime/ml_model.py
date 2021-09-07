851a85d5ab88901d2a15779228a3d5cd87780179
AntoineDidisheim <antoinedidisheim@gmail.com>
2021-09-07 19:16:05 +0200

    first run

--- theta_prime/ml_model.py
+++ theta_prime/ml_model.py
@@ -109,6 +109,7 @@ class NetworkMean:
         df=self._get_perf_oos()
         df.to_pickle(self.res_dir+f'perf_{year}.p')
         print(df.head())
+        print(df['pred'].std())
         # shapeley = self.shapeley_oos()
         # shapeley.to_pickle(self.res_dir+f'shap_{year}.p')
 
